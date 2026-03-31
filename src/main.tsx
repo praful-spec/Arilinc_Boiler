7aabfd07139886f74132a9f52ad67115ccfe494c
AriPrus <praful@faclon.com>
2026-03-31 22:21:47 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import AppRoutes from "./app/routes";
+import "./styles/global.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <AppRoutes />
+  </React.StrictMode>
 );
-
